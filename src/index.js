71db5e442784e73c5f20fbd6d25e3e1a8337b792
Austine Juma <austinejumah001@gmail.com>
2026-06-28 07:59:07 +0300

    feat: enable search functionality and display results in the console

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 import "./styles.css";
 import "./reset.css";
 
-console.log("Austine")
+import { getWeatherData } from "./weatherData.js";
+
+const input = document.querySelector("input#city");
+const fetchBtn = document.querySelector("form button");
+
+fetchBtn.addEventListener("click", async (e) => {
+    e.preventDefault();
+    const cityName = input.value;
+    try{
+        if(cityName){
+            const data = await getWeatherData(cityName);
+            console.log(data);
+            input.value = "";
+            input.focus();
+        }else{
+            input.focus();
+        } 
+        
+    }catch(error){
+        console.log(error.message)
+    }
+     
+})
+
