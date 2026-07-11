2f8fec1592b13b06160a3e2b9c10e294bf77dc7e
Austine Juma <austinejumah001@gmail.com>
2026-07-11 12:42:14 +0300

    refactor: improve code readability.
    wrap DOM rendering logic in descriptive functions and call them when
    needed.

--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@ import "./styles.css";
 import "./reset.css";
 
 import { getWeatherData } from "./weatherData.js";
+import { renderErrorMessage, renderWeatherData, showLoadingState } from "./dom.js";
 
 const input = document.querySelector("input#city");
 const fetchBtn = document.querySelector("form button");
+
 
 fetchBtn.addEventListener("click", async (e) => {
     e.preventDefault();
     const cityName = input.value;
     try{
         if(cityName){
+            showLoadingState();
             const data = await getWeatherData(cityName);
             console.log(data);
+            renderWeatherData(data)
+           
             input.value = "";
             input.focus();
         }else{
@@ -21,7 +26,9 @@ fetchBtn.addEventListener("click", async (e) => {
         
     }catch(error){
         console.log(error.message)
+        renderErrorMessage(error)
     }
      
 })
+
 
